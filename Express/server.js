33953ea6295b88f9f4ea37be3dc161005d813f0a
lijiahao <lijiahao@hitour.cc>
2016-10-11 17:51:50 +0800

    add expres cors

--- Express/server.js
+++ Express/server.js
@@ -2,7 +2,21 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var jwt    = require('jsonwebtoken');
-var config = require('./config'); 
+var config = require('./config');
+
+//allow custom header and CORS
+// app.all('*',function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+//   if (req.method == 'OPTIONS') {
+//     res.send(200); /让options请求快速返回/
+//   }
+//   else {
+//     next();
+//   }
+// }); 
 
 //body parser配置
 app.use(bodyParser.urlencoded({ extended: true }));
